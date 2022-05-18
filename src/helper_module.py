91e4cd4ae4b8d9305ef8239673c5e6b4efb59cc9
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-05-19 02:53:37 +0300

    Исправлен баг с конфигом

--- src/helper_module.py
+++ src/helper_module.py
@@ -1,18 +1,18 @@
 import datetime
-from src.cfgs import system_config as cfg
+from src.cfgs import system_config as scfg
 
 
 class Log:
     def __init__(self, key: str = 'LOG', comment: str = '') -> None:
-        if cfg.LOG_MODE:
-            with open(cfg.DATA_DIR + cfg.LOG_FILENAME, 'a') as f:
+        if scfg.LOG_MODE:
+            with open(scfg.DATA_DIR + scfg.LOG_FILENAME, 'a') as f:
                 f.write(f'{key} --- {str(datetime.datetime.now())} --- {comment}\n')
 
 
 class Debug:
     def __init__(self, data: any = '', is_log: bool = True, key: str = 'LOG'):
-        if cfg.DEBUG_MODE:
+        if scfg.DEBUG_MODE:
             print(data)
-        if cfg.LOG_MODE and is_log:
-            with open(cfg.DATA_DIR + cfg.LOG_FILENAME, 'a') as f:
+        if scfg.LOG_MODE and is_log:
+            with open(scfg.DATA_DIR + scfg.LOG_FILENAME, 'a') as f:
                 f.write(f'{key} --- {str(datetime.datetime.now())} --- {data}\n')
